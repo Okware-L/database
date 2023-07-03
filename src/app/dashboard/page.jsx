0cced4c051ce7aabadb0b713f482362d8eb62ed6
Okware-L <lewisokware@gmail.com>
2023-07-03 21:33:52 +0300

    avatars

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import User from '../../../public/user.png'
 import Next from '../../../public/next.png'
 
 
@@ -15,19 +16,34 @@ export default function Dashboard() {
     {/* Page content here */}
     
   <Navbar/>
-   <label htmlFor="my-drawer-2" className="drawer-button mt-5 lg:hidden">
+  <div className='flex flex-row my-3 justify-between'>
+  <div className=''>
+    <label htmlFor="my-drawer-2" className="drawer-button mt-5 lg:hidden">
     <Image
     src={Next}
     alt='arrow'
     className='w-5 h-5'
     />
    </label>
+   </div>
+
+<div class="flex -space-x-2 mx-1">
+  
+    <Image class="w-7 h-7 border-2 border-white rounded-full dark:border-gray-800" src={User} alt=""/>
+    <Image class="w-7 h-7 border-2 border-white rounded-full dark:border-gray-800" src={User} alt=""/>
+    <Image class="w-7 h-7 border-2 border-white rounded-full dark:border-gray-800" src={User} alt=""/>
+    <Image class="w-7 h-7 border-2 border-white rounded-full dark:border-gray-800" src={User} alt=""/>
+    <a class="flex items-center justify-center w-7 h-7 text-xs font-medium text-white bg-gray-700 border-1 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
+</div>
+
+  </div>
+  
   <Hero/>
  
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-70 h-full bg-base-200 text-base-content rounded-3xl m-2">
+    <ul className="menu p-4 w-70 h-full bg-base-200 text-base-content">
       {/* Sidebar content here */}
       <Sidebar/>
     </ul>
